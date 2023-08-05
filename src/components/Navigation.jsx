715b290ef5708049e6f22ea2d332175715c5f2fb
Akindejoye <tydruhill@gmail.com>
2023-08-05 04:34:01 +0100

    Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,15 +1,18 @@
 import { Navbar, Nav, Button, NavDropdown, Container } from "react-bootstrap";
+import "./Navigation.css";
+import { LinkContainer } from "react-router-bootstrap";
 
 const Navigation = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand>Ecomern</Navbar.Brand>
+        <LinkContainer to="/">
+          <Navbar.Brand>Ecommerce</Navbar.Brand>
+        </LinkContainer>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+          <Nav className="ms-auto">
+            <Nav.Link href="/login">Login</Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item>Dashboard</NavDropdown.Item>
               <NavDropdown.Item>Dashboard</NavDropdown.Item>

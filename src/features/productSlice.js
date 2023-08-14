243782bf565b131183678243106a2f0c2c0691ff
Akindejoye <tydruhill@gmail.com>
2023-08-14 18:45:38 +0100

    Basic slice setup

--- src/features/productSlice.js
+++ src/features/productSlice.js
@@ -1,1 +1,12 @@
 import { createSlice } from '@reduxjs/toolkit';
+import appApi from '../services/appApi';
+
+const initialState = [];
+
+export const productSlice = createSlice({
+  name: 'product',
+  initialState,
+  reducers: {},
+});
+
+export default productSlice.reducer;

b28baa88e1fff239667e1c598c1b599a286e0984
Akindejoye <tydruhill@gmail.com>
2023-08-14 19:29:43 +0100

    Redux Store setup

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,21 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// store setup
+import store from "./store";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import persistStore from "redux-persist/es/persistStore";
+
+// store persist
+const persistedStore = persistStore(store);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+    <React.StrictMode>
+      <PersistGate loading={<div>Loading...</div>} persistor={persistedStore}>
+        <App />
+      </PersistGate>
+    </React.StrictMode>
+  </Provider>
 );
